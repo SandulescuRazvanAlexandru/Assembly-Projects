fa8c060f38bc6804597c099e5925c0b87594dfe8
SandulescuRazvanAlexandru <sandulescu.razvan.alexandru@gmail.com>
2023-10-19 11:35:34 +0300

    Projects

---
+++ Procedures/proc.asm
@@ -0,0 +1,57 @@
+.model small
+.stack 16
+
+.data 
+    username db "HELLO"
+	dimUsername dw $ - username
+    password db "SECRET"
+	dimPassword dw $ - password
+	
+.code
+start:
+    mov ax, @data
+    mov ds, ax
+	
+	mov AX, offset username
+	push AX
+	mov AX, dimUsername
+	push AX
+	
+	call NEAR PTR addv
+	
+	mov AX, offset password
+	push AX
+	mov AX, dimPassword
+	push AX
+	
+	call NEAR PTR addv
+	
+	mov AX, 4c00h
+	int 21h
+	
+addv PROC NEAR
+	push BP
+	mov BP, SP
+	
+	push CX
+	
+	xor AX, AX
+	mov SI, 0
+	
+	mov CX, SS:[BP + 4]
+	mov BX, [BP + 6]
+	loop1:
+	
+		mov al, DS:[BX][SI]   	
+		xor ax, 3      			
+		mov DS:[BX][SI], al
+		add SI, 1
+		
+	loop loop1
+	
+	pop CX
+	pop BP
+	ret 4
+addv ENDP	
+	
+end start
